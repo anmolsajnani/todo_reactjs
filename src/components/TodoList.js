0b833b35349ddfc30ddbdcc877f3a7e13f3b1a21
anmolsajnani <43264013+anmolsajnani@users.noreply.github.com>
2019-07-05 11:31:58 +0530

    Forming TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,47 +1,63 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import {Table} from 'react-bootstrap';
 export default class TodoList extends React.Component{
     render(){
-        const {items,clearList,handleDelete,handleEdit,searcheditems,isSearch} = this.props
+        const {items,handleDelete,handleEdit,searcheditems,isSearch} = this.props
         return(
             <ul className="list-group my-5">
-                <h3 className="text-capitalize text-center">Todo List</h3>
-{isSearch ?
+            <Table bordered>
+            <thead>
+              <tr>
+                    <th>
+                        <li className="list-group-item justify-content-between" >Item
+                        {<i onClick={this.props.SortAlphaAsc} class="fa fa-sort-alpha-asc pl-2" aria-hidden="true"></i>}
+                        {<i onClick={this.props.SortAlphaDes} class="fa fa-sort-alpha-desc" aria-hidden="true"></i>}
+                        </li>
+                    </th>
+                    <th>
+                        <li className="list-group-item justify-content-between ">Date
+                        {<i onClick={this.props.SortAscDate} class="fa fa-sort-numeric-asc pl-2" aria-hidden="true"></i>}
+                        {<i  onClick={this.props.SortDesDate} class="fa fa-sort-numeric-desc" aria-hidden="true"></i>}
+                        </li>
+                    </th>
+                    <th>
+                        <li className="list-group-item justify-content-between ">Description</li>
+                    </th>
+                </tr>   
+            </thead>  
 
-    <div>
-    {items.map(item=>{
-        return(<TodoItem 
-            key={item.id}
-            name={item.name} 
-            title={item.title}
-            date={item.date}
-            handleDelete={()=>handleDelete(item.id)}
-            handleEdit={()=>handleEdit(item.id)}
-            />)
-    })
-}</div>
-:
-<div>
-{
-    searcheditems.map(item=>{
-        return(<TodoItem 
-            key={item.id}
-            name={item.name} 
-            title={item.title}
-            date={item.date}
-            handleDelete={()=>handleDelete(item.id)}
-            handleEdit={()=>handleEdit(item.id)}
-            />)
-    })
-}</div>}
+            {isSearch ?
 
-
-                
-                <button type="button" 
-                className="btn btn-danger btn-block text-capitalize mt-5"
-                onClick={clearList}>
-                    clear list
-                </button>
+                <tbody>     
+                    {items.map(item=>{
+                        return(<TodoItem 
+                                key={item.id}
+                                name={item.name} 
+                                title={item.title}
+                                date={item.date}
+                                handleDelete={()=>handleDelete(item.id)}
+                                handleEdit={()=>handleEdit(item.id)}
+                                />)
+                        })
+                    }
+                </tbody>
+                :
+                <tbody>
+                    {searcheditems.map(item=>{
+                        return(<TodoItem 
+                            key={item.id}
+                            name={item.name} 
+                            title={item.title}
+                            date={item.date}
+                            handleDelete={()=>handleDelete(item.id)}
+                            handleEdit={()=>handleEdit(item.id)}
+                            />)
+                    })
+                    }
+                </tbody>
+                }
+             </Table>
             </ul>
         );
     }

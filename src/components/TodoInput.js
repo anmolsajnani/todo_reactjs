953d594cc120f2ffe8fec0b6743ed7253984f0ca
anmolsajnani <43264013+anmolsajnani@users.noreply.github.com>
2019-07-05 11:30:26 +0530

    Input groups for todo

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -4,68 +4,50 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 export default class TodoInput extends React.Component{
   
-
     render(){
-
-        const{item,handleItem,handleChange,handleSubmit,editItem,handleSearch,ClickSearch,
-        startDate,endDate,handleChangeStart,handleChangeEnd,DateSearch,Sort,SortAlpha}=this.props
+        const{item,handleItem,handleChange,handleSubmit,editItem}=this.props
         return(
-            <div>
-            <div className="card card-body my-3">
-                <h3 className="text-capitalize text-center">Todo Input</h3>
+            <div className={"input_group_header"}>
+           
                 <form onSubmit={handleSubmit}>
-                    <div className="input-group">
-                        <div className="input-group-prepend">
-                            <div className="input-group-text bg-primary text-white">
-                                <i className="fas fa-book"/>
+                    <div className="input-group">  
+                        <input type="text" className="form-control form-control-sm "
+                            placeholder="Add a todo item"
+                            value={this.props.name}
+                            onChange={handleItem}/>
+                        <input type="text" className="form-control form-control-sm"
+                            placeholder="Description"
+                            value={this.props.title}
+                            onChange={handleChange}/>
+                            
+                            {editItem ? 
+                            <div className="input-group-prepend">
+                                <div className={
+                                    "input-group-text bg-success text-white"}>
+                                <i onClick={handleSubmit} class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                </div>
                             </div>
-                        </div>
-                    <input type="text" className="form-control"
-                        placeholder="Add a todo item"
-                        value={this.props.name}
-                        onChange={handleItem}/>
-                     <input type="text" className="form-control"
-                        placeholder="Description"
-                        value={this.props.title}
-                        onChange={handleChange}/>
+                            :
+                            <div className="input-group-prepend">
+                                <div className={
+                                    "input-group-text bg-primary text-white"}>
+                                <i onClick={handleSubmit} class=" fa fa-save" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                            }
                     </div>
-                    <Button type="submit" className={
-                        editItem 
-                        ? "btn btn-block btn-success mt-3":
-                        "btn btn-block btn-primary mt-3"
-                    }>
-                       {editItem ? "Edit Item": "Add Item"}</Button>
-
+                    
                     <div className="input-group">
-                <input type="text" className="form-control"
-                    placeholder="Search in the list.."
-                    onChange={handleSearch}/>
-                <Button onClick={ClickSearch} className={
-                    "btn btn-block btn-primary mt-3"
-                }>
-                   Search</Button>
-                   </div>
+                        <input class="form-control form-control-sm  w-75 pt-10" type="text" placeholder="Search"
+                        onChange={this.props.handleSearch} aria-label="Search"/>
+                        <a class="btn btn-primary btn-sm" onClick={this.props.ClickSearch}>
+                            <i class="fas fa-search" aria-hidden="true"></i>
+                        </a>
+                        <a class="btn btn-danger btn-sm" onClick={this.props.ClearList}>
+                            <i class="fa fa-trash-o"></i> Delete
+                        </a>
+                    </div>
                 </form>
-            </div>
-            
-            <Button onClick={Sort} className={
-                    "btn btn-block btn-primary mt-3"
-                }>
-                   Order by Date and Time</Button>
-                   <Button onClick={SortAlpha} className={
-                    "btn btn-block btn-primary mt-3"
-                }>
-                   Order by Item</Button>
-                   <DatePicker
-                    selected={startDate}
-                    selectsStart
-                     startDate={startDate}
-                     onChange={handleChangeStart}
-                        />
-                   
-                <Button className={
-                    "btn btn-block btn-primary mt-3"}
-                    onClick={()=> DateSearch(startDate)}>Search</Button> 
             </div>
         );
     }

22919fe32ce5ebc73be64c6f8f643c5f57ec5a7d
anmolsajnani <43264013+anmolsajnani@users.noreply.github.com>
2019-07-05 11:31:05 +0530

    DIsplay each todo item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,28 +5,29 @@ export default class TodoItem extends React.Component{
     render(){
         const {name,title,handleDelete,handleEdit,date}= this.props
         return(
-            <div>
-            
-            <Table><tr>
-            <td><li className="list-group-item text-capitalize  d-flex justify-content-between my-2">
-                <h6>{name}</h6></li></td>
-                <td><li className="list-group-item text-capitalize  d-flex justify-content-between my-2">
-                <h6>{date}</h6></li></td>
-                <td><li className="list-group-item text-capitalize  d-flex justify-content-between my-2">
-                <h6>{title}</h6>
-                <div className="todo-item">
-                    <span className="text-success" onClick={handleEdit}>
-                        <i className="fas fa-pen"></i>
-                    </span>
-                    <span className="mx-2 text-danger" onClick={handleDelete}>
-                        <i className="fas fa-trash"></i>
-                    </span>
-                </div>
-            </li></td>
-            
-            </tr>
-            </Table>
-            </div>
+            <tr>
+                <td>
+                    <li className="list-group-item text-capitalize  d-flex justify-content-between">
+                    <h6>{name}</h6></li> 
+                </td>
+                <td>
+                    <li className="list-group-item text-capitalize  d-flex justify-content-between ">
+                    <h6>{date}</h6></li>
+                </td>
+                <td>
+                    <li className="list-group-item text-capitalize  d-flex justify-content-between ">
+                    <h6>{title}</h6>
+                    <div className="todo-item">
+                        <span className="text-success" onClick={handleEdit}>
+                            <i className="fas fa-pen"></i>
+                        </span>
+                        <span className=" text-danger " onClick={handleDelete}>
+                            <i className="fa fa-times pl-2"></i>
+                        </span>
+                    </div>
+                    </li>
+                </td>
+            </tr>   
         );
     }
 }
